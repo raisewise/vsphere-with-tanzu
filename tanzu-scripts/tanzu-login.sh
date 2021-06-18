176dd8cebd6bf83e8c9f72d009f10b967da77f64
raisewise <raisewise@naver.com>
2021-06-18 15:34:50 +0900

    commit

--- tanzu-scripts/tanzu-login.sh
+++ tanzu-scripts/tanzu-login.sh
@@ -4,9 +4,14 @@ echo -e "Tanzu Namespace : \c "
 read -a NAMESPACE
 echo -e "Tanzu Cluster : \c "
 read -a CLUSTER
+echo -e "Password : \c"
+stty -echo
+read -a PWD
+stty echo
 
 SERVER='https://10.30.10.1'
 USER='administrator@vsphere.local'
+KUBECTL_VSPHERE_PASSWORD=$PWD
 #NAMESPACE='ss-infra'
 #CLUSTER='ss-infra-cluster'
 #SECURE_SKIP='--insecure-skip-tls-verify'
