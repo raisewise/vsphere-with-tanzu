f82037a622b9eed03d45a55dc58923b2f82a2263
raisewise <raisewise@naver.com>
2021-08-04 18:50:45 +0900

    commit

---
+++ tanzu-scripts/etcdctl.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This script assumes you're running from a master node
+# hosting the kube-apiserver.
+
+# Change this based on location of etcd nodes
+
+# ETCDCTL_API=3 etcdctl 
+#   --cert /etc/kubernetes/pki/apiserver-etcd-client.crt 
+#   --key /etc/kubernetes/pki/apiserver-etcd-client.key 
+#   --cacert /etc/kubernetes/pki/etcd/ca.crt 
+#   get /registry/secrets/default/security | hexdump -C
+
+# sample ./etcctl.sh get /registry/secrets/default/login1
+
+ENDPOINTS='127.0.0.1:2379'
+
+ETCDCTL_API=3 etcdctl \
+  --endpoints=${ENDPOINTS} \
+  --cacert="/etc/kubernetes/pki/etcd/ca.crt" \
+  --cert="/etc/kubernetes/pki/apiserver-etcd-client.crt" \
+  --key="/etc/kubernetes/pki/apiserver-etcd-client.key" \
+  ${@}
