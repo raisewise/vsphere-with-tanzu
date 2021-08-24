852dad2b2fca2678d70528452d4456161225d0a7
raisewise <raisewise@naver.com>
2021-08-24 09:30:27 +0900

    commit

--- tanzu-scripts/worker-ip.sh
+++ tanzu-scripts/worker-ip.sh
@@ -3,10 +3,13 @@
 echo -e "Tanzu Namespace : \c "
 read -a NAMESPACE
 TMP=$(kubectl config current-context)
+
 kubectl config use-context $NAMESPACE
+
 for i in `kubectl get virtualmachines | grep -v NAME | awk '{print $1}'`
 do
 echo $i; 
 kubectl get virtualmachines $i -o yaml | grep '^  vmIp'
 done
+
 kubectl config use-context $TMP
